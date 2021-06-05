00e6f3027b91da8d03297331d1559b94a8a96724
idemirli <ibrahimdemirli92@gmail.com>
2021-06-05 20:31:14 +0300

    deneme created

--- CoreDataSeeding/CoreDataSeeding/Controllers/HomeController.cs
+++ CoreDataSeeding/CoreDataSeeding/Controllers/HomeController.cs
@@ -20,6 +20,21 @@ namespace CoreDataSeeding.Controllers
 
         public IActionResult Index()
         {
+            string Deneme1 = string.Empty;
+            string Deneme2 = string.Empty;
+            string Deneme3 = string.Empty;
+            string Deneme4 = string.Empty;
+            string Deneme5 = string.Empty;
+            string Deneme6 = string.Empty;
+            string Deneme7 = string.Empty;
+            string Deneme8 = string.Empty;
+            string Deneme9 = string.Empty;
+            string Deneme10 = string.Empty;
+            string Deneme11 = string.Empty;
+            string Deneme12 = string.Empty;
+            string Deneme13 = string.Empty;
+            string Deneme14 = string.Empty;
+            string Deneme15 = string.Empty;
             return View();
         }
 
